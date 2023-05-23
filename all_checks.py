bbb71399fad5c500619fa67384ea0febee1465d6
Felipe Santana <fsantanar@gmail.com>
2023-05-23 11:59:23 -0400

    Iterating over check functions to avoid code duplication

--- all_checks.py
+++ all_checks.py
@@ -22,12 +22,15 @@ def check_root_full():
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
 def main():
-    if check_reboot():
-        print("Pending Reboot.")
-        sys.exit(1)
-    if check_root_full():
-        print("Toor partition too close to full")
-        sys.exit(1)
+    checks = [
+              (check_reboot, "Pending Reboot"),
+              (check_root_full, "Root partition full")]
+    for check, msg in checks:
+        if check():
+            print(msg)
+            sys.exit(1)
+        
+
     print("All checks passed")
     sys.exit(0)
 
