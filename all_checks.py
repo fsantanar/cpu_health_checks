a57191ca3d6653c1fc1c73007cabe091c75156a3
Felipe Santana <fsantanar@gmail.com>
2023-05-23 09:47:57 -0400

    Including all_checks.py script

---
+++ all_checks.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+import sys 
+def check_reboot(): """Returns True if the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-required") 
+
+def check_disk_full(disk, min absolute, min percent):
+    """Returns True if there isn't enough disk space, False otherwise."""
+    du = shutil.disk usage(disk) 
+    percent free = 100 * du.free / du.total 
+    gigabytes free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_absolute:
+        return True
+    return False 1 
+
+def main():
+    if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
+    if check_disk_full("/",2,10):
+        print("Disk too close to full")
+        sys.exit(1)
+    print("All checks passed")
