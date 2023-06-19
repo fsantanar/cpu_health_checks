a87a6a55746737ff0d9c88191dc646e29abaca7b
Felipe Santana <fsantanar@gmail.com>
2023-06-19 14:37:54 -0400

    changing linux github_env variable in workflow

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -6,10 +6,8 @@
 # Filename: test_checks.py
 # License: MIT License
 import os
-import sys
 import unittest
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import cpu_health_checks.cpu_health as cpu_health
 
 
