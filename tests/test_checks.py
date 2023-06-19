10d0aa631b31095da0d505a59e8ebbad0f06fa4a
Felipe Santana <fsantanar@gmail.com>
2023-06-19 14:28:56 -0400

    adding path to test_checks and ignoring level import error

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -9,9 +9,8 @@ import os
 import sys
 import unittest
 
-import cpu_health_checks.cpu_health as cpu_health
-
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import cpu_health_checks.cpu_health as cpu_health
 
 
 class SystemTestCase(unittest.TestCase):
