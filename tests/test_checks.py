ceace46adda8ada72e3f1aa2b7eea09bb3563894
Felipe Santana <fsantanar@gmail.com>
2023-06-29 17:19:25 -0400

    editing test file to retrieve more info on error 3

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -65,10 +65,10 @@ class SystemTestCase(unittest.TestCase):
                                      config_file=self.config_file_path,
                                      logs_folder=self.logs_folder_path)
         except Exception as e:
+            print(f"speed_log_filename: {self.cpu_check.speed_log_filename}")
             self.fail(f"Code execution failed with exception: {str(e)}")
 
-        print(f"speed_log_filename: {self.cpu_check.speed_log_filename}")
-        print(f"result: {result}")
+
 
         self.assertTrue(result['check_enough_disk_space'], 'check_enough_disk_space is not True')
         self.assertFalse(result['check_enough_idle_usage'], 'check_enough_idle_usage is not False')
